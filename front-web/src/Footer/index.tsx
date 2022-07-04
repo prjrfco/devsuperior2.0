cc5e36be649493f9e9f8ca4ae65d6907e5c5d7c5
Alluisio Silva <alluisio.silva@ipdec.org>
2022-07-04 15:15:26 -0400

    implementando novos ícones e modificações no footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { ReactComponent as YoutubeIcon } from "./youtube.svg";
+import { ReactComponent as GitHubIcon } from "../gitHub.svg";
 import { ReactComponent as LinkedinIcon } from "./linkedin.svg";
 import { ReactComponent as InstagramIcon } from "./instagram.svg";
 
@@ -8,13 +8,19 @@ function Footer() {
     <footer className="main-footer">
       App desenvolvido durante a aula de Projeto Integrado 2
       <div className="footer-icons">
-        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
-          <YoutubeIcon />
+        <a href="https://prjrfco.github.io/" target="_new">
+          <GitHubIcon />
         </a>
-        <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+        <a
+          href="https://www.linkedin.com/in/francisco-paes-ramos-junior-870b85183/"
+          target="_new"
+        >
           <LinkedinIcon />
         </a>
-        <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+        <a
+          href="https://www.instagram.com/francisco.junior.9828/"
+          target="_new"
+        >
           <InstagramIcon />
         </a>
       </div>
